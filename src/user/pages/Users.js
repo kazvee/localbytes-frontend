c3ef8a7f41e9b78ddc07c2f1114a46883eebfbc0
karvok <kayvee123@gmail.com>
2022-12-26 12:19:35 -0700

    Add UsersList Page/Component & UserItem Component

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
+import UsersList from '../components/UsersList';
+
 const Users = () => {
-  return <h2>Users Page</h2>;
+  const USERS = [
+    {
+      id: 'u1',
+      name: 'Little Kitten',
+      image: 'http://placekitten.com/200/300',
+      places: '3',
+    },
+  ];
+
+  return <UsersList items={USERS} />;
 };
 
 export default Users;
