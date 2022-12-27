5a2e46cba7f9298cb3ff19e96506e1c73d9b9033
karvok <kayvee123@gmail.com>
2022-12-27 10:09:56 -0700

    Add multiple inputs

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,19 +1,37 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 
 import Input from '../../shared/components/FormElements/Input';
-import { VALIDATOR_REQUIRE } from '../../shared/util/validators';
+import {
+  VALIDATOR_MINLENGTH,
+  VALIDATOR_REQUIRE,
+} from '../../shared/util/validators';
 
 import './NewPlace.css';
 
 const NewPlace = () => {
+  const titleInputHandler = useCallback((id, value, isValid) => {}, []);
+
+  const descriptionInputHandler = useCallback((id, value, isValid) => {}, []);
+
   return (
     <form className='place-form'>
       <Input
+        id='title'
         element='input'
         type='text'
         label='Title'
         validators={[VALIDATOR_REQUIRE()]}
         errorText='Please enter a valid Title!'
+        onInput={titleInputHandler}
+      />
+
+      <Input
+        id='descrption'
+        element='textarea'
+        label='Description'
+        validators={[VALIDATOR_MINLENGTH(5)]}
+        errorText='Please enter a valid Description (of at least 5 characters)!'
+        onInput={descriptionInputHandler}
       />
     </form>
   );
