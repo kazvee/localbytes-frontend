f98a2088b95f53a73c8459622b59e66fbf23a4d7
karvok <kayvee123@gmail.com>
2023-01-27 23:08:15 -0700

    Implement image upload for new Places

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -5,6 +5,7 @@ import Input from '../../shared/components/FormElements/Input';
 import Button from '../../shared/components/FormElements/Button';
 import ErrorModal from '../../shared/components/UIElements/ErrorModal';
 import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner';
+import ImageUpload from '../../shared/components/FormElements/ImageUpload';
 import {
   VALIDATOR_MINLENGTH,
   VALIDATOR_REQUIRE,
@@ -32,6 +33,11 @@ const NewPlace = () => {
         value: '',
         isValid: false,
       },
+
+      image: {
+        value: null,
+        isValid: false,
+      },
     },
     false
   );
@@ -42,17 +48,13 @@ const NewPlace = () => {
     event.preventDefault();
 
     try {
-      await sendRequest(
-        'http://localhost:5000/api/places',
-        'POST',
-        JSON.stringify({
-          title: formState.inputs.title.value,
-          description: formState.inputs.description.value,
-          address: formState.inputs.address.value,
-          creator: auth.userId,
-        }),
-        { 'Content-Type': 'application/json' }
-      );
+      const formData = new FormData();
+      formData.append('title', formState.inputs.title.value);
+      formData.append('description', formState.inputs.description.value);
+      formData.append('address', formState.inputs.address.value);
+      formData.append('creator', auth.userId);
+      formData.append('image', formState.inputs.image.value);
+      await sendRequest('http://localhost:5000/api/places', 'POST', formData);
       history.push('/');
     } catch (err) {}
   };
@@ -85,6 +87,11 @@ const NewPlace = () => {
           label='Address'
           validators={[VALIDATOR_REQUIRE()]}
           errorText='Please enter a valid Address!'
+          onInput={inputHandler}
+        />
+        <ImageUpload
+          id='image'
+          errorText='Please select an image!'
           onInput={inputHandler}
         />
         <Button type='submit' disabled={!formState.isValid}>
